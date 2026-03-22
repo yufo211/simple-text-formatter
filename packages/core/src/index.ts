e6a956f5ebb9b7b259cbc98ee312cccd8be954c9
u-hossy <95125392+u-hossy@users.noreply.github.com>
2026-03-22 11:43:18 +0900

    WIP: 型定義の追加

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,50 @@
+/** スキーマのバージョン */
+type SchemaVersion = 1;
+
+// ── 共通のベース型 ────────────────────────────────────────
+
+interface ProcessBase {
+  label?: string;
+  enabled: boolean;
+}
+
+// ── 各プロセスの型 ──────────────────────────────────────
+
+interface ReplaceProcess extends ProcessBase {
+  type: "replace";
+  from: string;
+  to: string;
+  useRegex: boolean;
+}
+
+interface HalfToFullProcess extends ProcessBase {
+  type: "half-to-full";
+  target: ConvertTarget;
+}
+
+interface FullToHalfProcess extends ProcessBase {
+  type: "full-to-half";
+  target: ConvertTarget;
+}
+
+interface ConvertTarget {
+  number: boolean;
+  space: boolean;
+  symbol: boolean;
+}
+
+// ── Union型 ──────────────────────────────────────────────
+
+type Process = ReplaceProcess | HalfToFullProcess | FullToHalfProcess;
+
+// ── ルートの型 ───────────────────────────────────────────
+
+interface ReplacementSchema {
+  schemaVersion: SchemaVersion;
+  label: string;
+  processes: Process[];
+}
+
 /**
  * テキストの整形
  *
@@ -5,7 +52,8 @@
  * @param config 整形設定
  * @returns 整形済みテキスト
  */
-export const formatText = (text: string, config: object): string => {
+export const formatText = (text: string, config: ReplacementSchema): string => {
+  console.log(config);
   return text.trim().toUpperCase();
 };
 
