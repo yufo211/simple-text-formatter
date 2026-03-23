48e316d39d56ad7d54564dc526796f78e9d5c98b
u-hossy <95125392+u-hossy@users.noreply.github.com>
2026-03-24 00:43:56 +0900

    WIP: Add shadcn-ui and others

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./global.css";
 import App from "./App.tsx";
 
+// biome-ignore lint/style/noNonNullAssertion: <div id="root"></div> exists in index.html
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
