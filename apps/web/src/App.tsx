4f284244a5b2dca0421eaf72a64d06fea678933f
u-hossy <95125392+u-hossy@users.noreply.github.com>
2026-03-27 11:26:22 +0900

    Implement basic layout

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,12 +1,23 @@
-import { Button } from "./components/ui/button";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
 
 function App() {
   return (
-    <>
-      <p>hello</p>
-      <p>from App.tsx</p>
-      <Button>aaa</Button>
-    </>
+    <div className="flex flex-col h-screen min-h-96">
+      <header className="h-14 shrink-0 px-6 border-b border-border">aaa</header>
+      <main className="flex-1 min-h-0">
+        <ResizablePanelGroup orientation="vertical">
+          <ResizablePanel>One</ResizablePanel>
+          <ResizableHandle />
+          <ResizablePanel>Two</ResizablePanel>
+          <ResizableHandle />
+          <ResizablePanel>Three</ResizablePanel>
+        </ResizablePanelGroup>
+      </main>
+    </div>
   );
 }
 
