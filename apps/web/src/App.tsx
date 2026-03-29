0f71d20598b41aeb7bcae3a10942e992fbe02113
u-hossy <95125392+u-hossy@users.noreply.github.com>
2026-03-29 16:35:19 +0900

    Implement dark mode

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -2,8 +2,10 @@ import type { Process } from "@simple-text-formatter/core";
 import { formatText } from "@simple-text-formatter/core";
 import { useMemo, useState } from "react";
 import InputArea from "@/components/InputArea";
+import { ModeToggle } from "@/components/mode-toggle";
 import OutputArea from "@/components/OutputArea";
 import ProcessConfigArea from "@/components/ProcessConfigArea";
+import { ThemeProvider } from "@/components/theme-provider";
 import {
   ResizableHandle,
   ResizablePanel,
@@ -22,27 +24,34 @@ function App() {
   }, [inputText, processes]);
 
   return (
-    <div className="flex flex-col h-screen min-h-fit">
-      <header className="h-14 shrink-0 px-6 border-b border-border">aaa</header>
-      <main className="flex-1 min-h-0">
-        <ResizablePanelGroup orientation="vertical">
-          <ResizablePanel minSize={20} className="px-2 md:px-6 py-1 my-2">
-            <InputArea inputText={inputText} setInputText={setInputText} />
-          </ResizablePanel>
-          <ResizableHandle withHandle />
-          <ResizablePanel minSize={20} className="px-2 md:px-6 py-1 my-2">
-            <ProcessConfigArea
-              processes={processes}
-              setProcesses={setProcesses}
-            />
-          </ResizablePanel>
-          <ResizableHandle withHandle />
-          <ResizablePanel minSize={20} className="px-2 md:px-6 py-1 my-2">
-            <OutputArea convertedText={convertedText} />
-          </ResizablePanel>
-        </ResizablePanelGroup>
-      </main>
-    </div>
+    <ThemeProvider storageKey="vite-ui-theme">
+      <div className="flex flex-col h-screen min-h-fit">
+        <header className="flex flex-row items-center justify-between h-14 shrink-0 px-2 md:px-6 border-b border-border">
+          <div></div>
+          <div>
+            <ModeToggle />
+          </div>
+        </header>
+        <main className="flex-1 min-h-0">
+          <ResizablePanelGroup orientation="vertical">
+            <ResizablePanel minSize={20} className="px-2 md:px-6 py-1 my-2">
+              <InputArea inputText={inputText} setInputText={setInputText} />
+            </ResizablePanel>
+            <ResizableHandle withHandle />
+            <ResizablePanel minSize={20} className="px-2 md:px-6 py-1 my-2">
+              <ProcessConfigArea
+                processes={processes}
+                setProcesses={setProcesses}
+              />
+            </ResizablePanel>
+            <ResizableHandle withHandle />
+            <ResizablePanel minSize={20} className="px-2 md:px-6 py-1 my-2">
+              <OutputArea convertedText={convertedText} />
+            </ResizablePanel>
+          </ResizablePanelGroup>
+        </main>
+      </div>
+    </ThemeProvider>
   );
 }
 
